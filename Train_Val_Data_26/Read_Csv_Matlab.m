e6885ab8559355c3bd6cae4dd90091cb7d8c5e1a
onlybuzy <josezuncaz@gmail.com>
2024-06-28 11:08:42 -0500

    correction of data

---
+++ Train_Val_Data_26/Read_Csv_Matlab.m
@@ -0,0 +1,9 @@
+%% Training Data
+
+C1_R_T=readmatrix("Train\C1_vr_train.csv");
+C1_L_T=readmatrix("Train\C1_vl_train.csv");
+
+%% Validation Data
+
+C1_R_V=readmatrix("Validation\C1_vr_val.csv");
+C1_L_V=readmatrix("Validation\C1_vl_val.csv");
